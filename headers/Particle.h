302b8267ca323da2aa800fe38aa2c8d18738b4d8
seif-sallam <seif_sallam@aucegypt.edu>
2022-02-12 11:57:16 +0200

    Highlighting Particle and Random movement

--- headers/Particle.h
+++ headers/Particle.h
@@ -4,6 +4,7 @@
 class Particle
 {
     sf::RectangleShape m_Shape;
+    bool m_IsColliding;
 
 public:
     Particle() = default;
@@ -12,4 +13,9 @@ public:
     const sf::Vector2f GetPosition() const;
     const sf::Vector2f GetSize() const;
     const sf::FloatRect GetBoundingBox() const;
+    void RandomMove();
+    bool IsColliding(const sf::FloatRect &other);
+
+private:
+    void Highlight();
 };
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1,4 +1,6 @@
 #include "Particle.h"
+#include "Random.h"
+#include "Application.h"
 
 Particle::Particle(const sf::Vector2f &position, const sf::Vector2f &size)
 {
@@ -24,4 +26,34 @@ const sf::Vector2f Particle::GetSize() const
 const sf::FloatRect Particle::GetBoundingBox() const
 {
     return m_Shape.getGlobalBounds();
+}
+
+void Particle::RandomMove()
+{
+    sf::Vector2f dir;
+    dir.x = Random::GetRandomFloat(-3.0f, 3.0f);
+    dir.y = Random::GetRandomFloat(-3.0f, 3.0f);
+    sf::Vector2u windowRes = Application::GetWindowRes();
+    m_Shape.move(dir);
+    if (m_Shape.getPosition().x > windowRes.x - 5.f)
+        m_Shape.setPosition(5.0f, m_Shape.getPosition().y);
+    if (m_Shape.getPosition().x < 5.0f)
+        m_Shape.setPosition(windowRes.x - 5.0f, m_Shape.getPosition().y);
+    if (m_Shape.getPosition().y > windowRes.y - 5.f)
+        m_Shape.setPosition(m_Shape.getPosition().x, 5.0f);
+    if (m_Shape.getPosition().y < 5.0f)
+        m_Shape.setPosition(m_Shape.getPosition().x, windowRes.y - 5.0f);
+}
+
+bool Particle::IsColliding(const sf::FloatRect &other)
+{
+    m_IsColliding = m_Shape.getLocalBounds().intersects(other);
+    Highlight();
+    return m_IsColliding;
+}
+
+void Particle::Highlight()
+{
+    int color = (m_IsColliding) ? 255 : 125;
+    m_Shape.setFillColor(sf::Color(color, color, color, 255));
 }
